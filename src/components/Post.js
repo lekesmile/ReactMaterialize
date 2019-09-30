abc4b88a6640fe4e8b5fa26a284f2c19b28d9390
Igunnuoda Olorunleke <s1900550@edu.bc.fi>
2019-09-30 08:36:22 +0300

    material

--- src/components/Post.js
+++ src/components/Post.js
@@ -17,7 +17,7 @@ const useStyles = makeStyles({
 });
 
 const postCard = [
-    {   
+    {
         textmain: "An apple is a sweet, edible fruit produced by an apple tree (Malus domestica). Apple trees are cultivated worldwide and are the most widely grown species in the genus Malus. The tree originated in Central Asia, where its wild ancestor, Malus sieversii, is still found today.",
         image: "https://ripeme.com/wp-content/uploads/RF-10012-RIPE-ORGANIC-ORGANIC-BANANAS.jpg",
         text: "Banana",
@@ -29,9 +29,9 @@ const postCard = [
         text: "Orange",
         id: 2
     },
-    {   
+    {
         textmain: "Kiwifruit (often abbreviated as kiwi outside Australia and New Zealand), or Chinese gooseberry, is the edible berry of several species of woody vines in the genus Actinidia.[1][2] The most common cultivar group of kiwifruit",
-        image:"https://www.wonderopolis.org/wp-content/uploads/2012/10/Kiwi_shutterstock_84220885-1.jpg",
+        image: "https://www.wonderopolis.org/wp-content/uploads/2012/10/Kiwi_shutterstock_84220885-1.jpg",
         text: "Kiwi",
         id: 3
     },
@@ -46,43 +46,43 @@ const postCard = [
 const Post = () => {
     const classes = useStyles()
     return (
-        <div style={{marginTop:150, padding:20}}> 
-        <Grid container direction="row" justify="center" alignItems="center" >
-        {postCard.map( item => 
-        <Grid item key={item.id}>
-            <Card className={classes.card}>
-                <CardActionArea> 
-               
-                    <CardMedia
-                        component="img"
-                        alt="fruits"
-                        height="140"
-                        image={item.image} 
-                        title={item.text}
-                       
-                    />
-                    <CardContent>
-                        <Typography gutterBottom variant="h5" component="h2">
-                           {item.text}
-          </Typography>
-                        <Typography variant="body2" color="textSecondary" component="p">
-                            {item.textmain}
-          </Typography>
-                    </CardContent>
-                </CardActionArea>
-                <CardActions>
-                    <Button size="small" color="primary">
-                        Share
+        <div style={{ marginTop: 150, padding: 20 }}>
+            <Grid container direction="row" justify="center" alignItems="center" >
+                {postCard.map(item =>
+                    <Grid item key={item.id}>
+                        <Card className={classes.card}>
+                            <CardActionArea>
+
+                                <CardMedia
+                                    component="img"
+                                    alt="fruits"
+                                    height="140"
+                                    image={item.image}
+                                    title={item.text}
+
+                                />
+                                <CardContent>
+                                    <Typography gutterBottom variant="h5" component="h2">
+                                        {item.text}
+                                    </Typography>
+                                    <Typography variant="body2" color="textSecondary" component="p">
+                                        {item.textmain}
+                                    </Typography>
+                                </CardContent>
+                            </CardActionArea>
+                            <CardActions>
+                                <Button size="small" color="primary">
+                                    Share
         </Button>
-                    <Button size="small" color="primary">
-                        Learn More
+                                <Button size="small" color="primary">
+                                    Learn More
         </Button>
-                </CardActions>
-              
-            </Card>
+                            </CardActions>
+
+                        </Card>
+                    </Grid>
+                )}
             </Grid>
-              )}
-              </Grid>
         </div>
     )
 }
